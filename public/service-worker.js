214eaacc4adff5fe92a4422ca8f6d642915c3d80
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-11-22 23:27:30 -0500

    icon routes for cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,8 +5,14 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/manifest.webmanifest",
   "/styles.css",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+  "/assets/images/icons/icon-72x72.png",
+  "/assets/images/icons/icon-96x96.png",
+  "/assets/images/icons/icon-128x128.png",
+  "/assets/images/icons/icon-144x144.png",
+  "/assets/images/icons/icon-152x152.png",
+  "/assets/images/icons/icon-192x192.png",
+  "/assets/images/icons/icon-384x384.png",
+  "/assets/images/icons/icon-512x512.png",
   "/db.js",
   "/index.js"
 
