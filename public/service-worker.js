e265704b7b01e41f2f14603464e51b4a6d02b63e
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-11-22 23:37:16 -0500

    Mobile Wallet is an installable PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,7 +22,6 @@ const FILES_TO_CACHE = [
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log("Your files were pre-cached successfully!");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
